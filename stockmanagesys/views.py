a4719ed866aed72f0c060a66d3aee83c17359386
DisneyVertudez <disney.vertudez@zdspgc.edu.ph>
2023-03-20 18:09:19 +0800

    template added for our system

--- stockmanagesys/views.py
+++ stockmanagesys/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	title = 'Welcome: This is the Home Page'
+	form = 'Welcome: This is the Home Page'
+	context = {
+	"title": title,
+	"test": form,
+	}
+	return render(request, "home.html",context)
