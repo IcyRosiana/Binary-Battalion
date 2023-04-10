6b1d7e85256d001c68bb40ea63dc2fe7bf1071e6
Xphyerion <reycenen.pintor@zdspgc.edu.ph>
2023-04-10 20:35:00 +0800

    models & admin page

--- stockmanagesys/models.py
+++ stockmanagesys/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+	category = models.CharField(max_length=50, blank=True, null=True)
+	item_name = models.CharField(max_length=50, blank=True, null=True)
+	quantity = models.IntegerField(default='0', blank=True, null=True)
+	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
+	receive_by = models.CharField(max_length=50, blank=True, null=True)
+	issue_quantity = models.IntegerField(default='0', blank=True, null=True)
+	issue_by = models.CharField(max_length=50, blank=True, null=True)
+	issue_to = models.CharField(max_length=50, blank=True, null=True)
+	phone_number = models.CharField(max_length=50, blank=True, null=True)
+	created_by = models.CharField(max_length=50, blank=True, null=True)
+	reorder_level = models.IntegerField(default='0', blank=True, null=True)
+	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	export_to_CSV = models.BooleanField(default=False)
+
+
+	def __str__(self):
+		return self.item_name	
